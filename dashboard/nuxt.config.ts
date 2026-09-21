b982c7b83faf2f3c9a014a23c65d266f720d388c
DDlia <gh@ddlia.com>
2026-09-21 17:29:13 +0900

    feat(dashboard): configure project favicon in frontend

--- dashboard/nuxt.config.ts
+++ dashboard/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'RTMP Live Dashboard',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  }
 })
